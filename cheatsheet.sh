3cdef7098ed7bca7c09a90c0892d80f3941a83eb
Jakowicz <simon@jakowicz.com>
2018-12-22 02:21:25 +0000

    updated cheatsheet to be one file

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-source print.sh
+SJG_RED="\033[0;31m"
+SJG_YELLOW='\033[1;33m'
+SJG_NC='\033[0m'
+CHEATSHEET=$1
 
-cheatsheets=()
+function sj_gash_print_title() {
+  printf "$SJG_YELLOW $1\n"
+}
 
-for cheatsheet in `ls -1 gash`; do
-  cheatsheets+=( `echo ${cheatsheet/.sh/}` )
-done
+function sj_gash_print_body() {
+  printf "$SJG_NC $1\n\n"
+}
+
+cheatsheets="gpg python"
 
 echo ""
 
-if [ -z $1 ]; then
-  printf "$SJG_RED No program provided, available options are: %s" "${cheatsheets[*]}"
-elif [ ! -f gash/$1.sh ]; then
-  printf "$SJG_RED Couldn't find a cheatsheet for $1"
+if [ -z $CHEATSHEET ]; then
+  printf "$SJG_RED No program provided, available options are: $cheatsheets"
 else
-  source gash/$1.sh
-  gash
+  if [ $CHEATSHEET == "gpg" ]; then
+    sj_gash_print_title Retrieve key
+    sj_gash_print_body "gpg --receive-keys simon@jakowicz.com"
+
+    sj_gash_print_title "Encrypt using my private key"
+    sj_gash_print_body "gpg -e -r simon@jakowicz.com -o/n"
+
+    sj_gash_print_title "Decrypt symmetric or asymmetric"
+    sj_gash_print_body "gpg -d -o"
+
+    sj_gash_print_title "Symmetric Encryption"
+    sj_gash_print_body "gpg --symmetric"
+  elif [ $CHEATSHEET == "python" ]; then
+    sj_gash_print_title "Start HTTP server Python 2"
+    sj_gash_print_body "python -m SimpleHTTPServer 8000"
+
+    sj_gash_print_title "Start HTTP server Python 3"
+    sj_gash_print_body "python -m http.server 3000"
+  else
+      printf "$SJG_RED Could not find cheatsheet for: $CHEATSHEET"
+  fi
 fi
--- gash/python.sh
+++
@@ -1,7 +0,0 @@
-function gash() {
-  sj_gash_print_title "Start HTTP server Python 2"
-  printsj_gash_print_body2 "python -m SimpleHTTPServer 8000"
-
-  sj_gash_print_title "Start HTTP server Python 3"
-  sj_gash_print_body "python -m http.server 3000"
-}
